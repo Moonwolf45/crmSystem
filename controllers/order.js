2195f738a7a6e4b2158214630be0baa06ffc1ad8
Vladislav <miznikov.1995@mail.ru>
2018-12-18 20:03:24 +0500

    Добавлены все методы для работы с заказами

--- controllers/order.js
+++ controllers/order.js
@@ -4,11 +4,33 @@ const errorHandler = require('../utils/errorHandler');
 
 
 module.exports.getAllOrder = async function (req, res) {
+    const query = {
+        user: req.user.id
+    };
+
+    if (req.query.start) {
+        query.date = {
+            $gte: req.query.start
+        };
+    }
+
+    if (req.query.end) {
+        if (!query.date) {
+            query.date = {};
+        }
+
+        query.date['$lte'] = req.query.end;
+    }
+
+    if (req.query.order) {
+        query.order = +req.query.order;
+    }
+
     try {
-        const order = await Order.find({
-            user: req.user.id
-        });
-        res.status(200).json(order);
+        const orders = await Order.find(query).sort({
+            date: -1
+        }).skip(+req.query.offset).limit(+req.query.limit);
+        res.status(200).json(orders);
     } catch (e) {
         errorHandler(res, e);
     }
@@ -16,15 +38,18 @@ module.exports.getAllOrder = async function (req, res) {
 
 module.exports.addOrder = async function (req, res) {
     try {
+        const lastOrder = Order.findOne({
+            user: req.user.id
+        }).sort({
+            date: -1
+        });
+
+        const maxOrder = lastOrder ? lastOrder.order : 0;
+
         const order = await new Order({
-            date: req.body.date,
             order: req.body.order,
-            list: [{
-                name: '',
-                quantity: '',
-                cost: ''
-            }],
-            user_id: req.user.id
+            list: req.body.list,
+            user_id: maxOrder + 1
         }).save();
         res.status(201).json(order);
     } catch (e) {
