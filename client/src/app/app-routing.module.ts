e1216f07d0c27c2e06dc0c9661bef745f84b4788
Vladislav <miznikov.1995@mail.ru>
2019-01-04 17:00:02 +0500

    Создан компонент регистрации, добавлены layout для авторизации и сайта, установлен favicon, настроен шаблон авторизации.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
 
 const routes: Routes = [
-    {path: 'login', component: LoginPageComponent}
+    {path: '', component: AuthLayoutComponent, children: [
+        {path: '', redirectTo: '/login', pathMatch: 'full'},
+        {path: 'login', component: LoginPageComponent},
+        {path: 'registration', component: RegistrationPageComponent}
+    ]},
+    {path: '', component: SiteLayoutComponent, children: [
+
+    ]}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
+import {RegistrationPageComponent} from './registration-page/registration-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginPageComponent,
+        AuthLayoutComponent,
+        SiteLayoutComponent,
+        RegistrationPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
